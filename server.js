cb53faaa3fa3c53d5a030c14d7e9e73d88ddc9bb
filipkaduch <filip.kaduch@ataccama.com>
2023-05-11 01:39:04 +0200

    feat: test comunica

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const queryEngine = require('@comunica/query-sparql').QueryEngine;
-const newEngine = new queryEngine();
+const myEngine = new queryEngine();
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = 3000;
 
-const myEngine = newEngine({ config: 'config/config.json' });
+// const myEngine = newEngine({ config: 'config/config.json' });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
