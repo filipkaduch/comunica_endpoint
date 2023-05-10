cc0820a374a16f86413d82122dcdbbb3b479c627
filipkaduch <filip.kaduch@ataccama.com>
2023-05-10 23:23:41 +0200

    feat: test comunica

--- server/server.js
+++ server/server.js
@@ -1,41 +1,23 @@
+const newEngine = require('@comunica/actor-init-sparql').newEngine;
 const express = require('express');
-const {ActorInitSparql} = require('@comunica/actor-init-sparql');
-
 const app = express();
+const bodyParser = require('body-parser');
+const port = 3000;
 
-// Set up Comunica
-const comunicaConfig = {
-    '@context': [
-        'https://linkedsoftwaredependencies.org/bundles/npm/@comunica/actor-init-sparql/^2.1.0/components/context.jsonld',
-        {
-            'mySparqlEndpoint': {
-                '@id': 'cax://mySparqlEndpoint',
-                '@type': '@id',
-            },
-        },
-    ],
-    'mySparqlEndpoint': {
-        '@id': 'https://malware-ontology.onrender.com/sparql',
-        '@type': 'mySparqlEndpoint',
-    },
-};
-const comunica = new ActorInitSparql({config: comunicaConfig});
+const myEngine = newEngine({ config: 'config/config.json' });
 
-// Define a route that queries your ontology using Comunica
-app.get('/sparql', async (req, res) => {
-    const queryResult = await comunica.query(req.query.query);
-    const bindingsStream = queryResult.bindingsStream;
-    bindingsStream.on('data', (bindings) => {
-        // Do something with each binding
-        console.log(bindings.toObject());
-    });
-    bindingsStream.on('end', () => {
-        // Send the result as JSON
-        res.json(queryResult);
-    });
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/query', async (req, res) => {
+    const query = req.body.query;
+    try {
+        const result = await myEngine.query(query, { sources: 'config/config.json' });
+        const bindings = await result.bindings();
+        res.json(bindings.map(binding => binding.toObject()));
+    } catch (err) {
+        res.status(400).send(err.message);
+    }
 });
 
-// Start the server
-app.listen(3000, () => {
-    console.log('Server listening on port 3000');
-});
+app.listen(port, () => console.log(`Comunica server listening on port ${port}`));
